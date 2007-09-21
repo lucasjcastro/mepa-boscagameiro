20b804edba5629954831f9dc717c6e083abe36e4
lucasgameiro <lucasgameiro@b9db43d8-a73a-0410-8382-8b98c2212edd>
2007-09-21 17:59:18 +0000

    resolvi o pau que deu nos updates

--- trunk/Mepa.cpp
+++ trunk/Mepa.cpp
@@ -1,5 +1,6 @@
 #include "Mepa.h"
-#include <fstream>
+
+/* ---- Inicio dos metodos publicos ---- */
 
 Mepa::Mepa( const char* vNome )
 {
@@ -13,26 +14,34 @@ Mepa::~Mepa()
 {
 }
 
+/* ---- Fim dos metodos publicos ---- */
+
+/* ---- Inicio dos metodos privados ---- */
+
 void 
 Mepa::proximaInstrucao()
 {
 	this->mPC++;
 }
-		
+
 void
-Mepa::proximaInstrucao(int vNovaPos)
+Mepa::proximaInstrucao(int vNovaPosicao)
 {
-	this->mPC = vNovaPos;
+	this->mPC = vNovaPosicao;
 }
 
 void
 Mepa::pushM()
 {
-	
+	mS++;
+	mM.push_back(VAZIO);
 }
 
 void
 Mepa::CRCT(int vK)
 {
-	
+	this->pushM();
+	mM[mS] = vK;
 }
+
+/* ---- Fim dos metodos privados ---- */
