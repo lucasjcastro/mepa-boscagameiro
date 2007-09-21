b56eb4f6d2537871e02181a0a777e0c0a51f5104
lucasgameiro <lucasgameiro@b9db43d8-a73a-0410-8382-8b98c2212edd>
2007-09-21 18:12:50 +0000

    assinatura dos metodos

--- trunk/Mepa.h
+++ trunk/Mepa.h
@@ -42,6 +42,12 @@ class Mepa
 		pushM();
 		
 		/**
+		 * Desaloca uma posicao na pilha M e decrementa S
+		 */
+		void
+		popM();
+		
+		/**
 		 * Incrementa o ponteiro do PC, se possivel
 		 */
 		void
@@ -56,10 +62,101 @@ class Mepa
 		
 		/**
 		 * Carrega a constante na pilha
-		 * @param constante a ser carregada
+		 * @param k constante a ser carregada
 		 */
 		void
 		CRCT(int k);
+		
+		/**
+		 * Carrega Valor n no topo da pilha
+		 * @param n valor a ser carregado na pilha
+		 */
+		void
+		CRVL(int n);
+		
+		/**
+		 * Somar
+		 */
+		void
+		SOMA();
+		
+		/**
+		 * Subtrair
+		 */
+		void
+		SUBT();
+		
+		/**
+		 * Multiplicar
+		 */
+		void
+		MULT();
+		
+		/**
+		 * Divisao Inteira
+		 */
+		void
+		DIV();
+		
+		/**
+		 * Inverter Sinal
+		 */
+		void
+		INVR();
+		
+		/**
+		 * Conjuncao
+		 */
+		void
+		CONJ();
+		
+		/**
+		 * Disjuncao
+		 */
+		void
+		DISJ();
+		
+		/**
+		 * Negacao
+		 */
+		void
+		NEGA();
+		
+		/**
+		 * Comparar Menor
+		 */
+		void
+		CMME();
+		
+		/**
+		 * Comparar Maior
+		 */
+		void
+		CMMA();
+		
+		/**
+		 * Comparar igual
+		 */
+		void
+		CMIG();
+		
+		/**
+		 * Comparar Desigual
+		 */
+		void
+		CMDG();
+		
+		/**
+		 * Comparar menor ou igual
+		 */
+		void
+		CMEG();
+		
+		/**
+		 * Comparar maior ou igual
+		 */
+		void
+		CMAG();
 	
 	public:
 	
