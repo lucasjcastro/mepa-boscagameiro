96eceff03193ff87823cc98a5f991de804b0d90d
leandroboscariol <leandroboscariol@b9db43d8-a73a-0410-8382-8b98c2212edd>
2007-09-23 20:56:25 +0000

    Opcoes de entrada quase totalmente funcionais! :)

--- main.cpp
+++ main.cpp
@@ -2,6 +2,144 @@
 
 int main(int argc, char *argv[])
 {
-	std::cout << "Hello Mepa!" << std::endl;
+	if ( argc < 2 )
+	{
+		std::cout << "Numero insuficiente de parametros. " <<
+				"Digite 'mepa -?' para a lista completa de opcoes." << std::endl;
+		return 0;
+	}
+	int
+	numRegisBase;
+	
+	int
+	tamMemPrograma;
+	
+	int
+	tamMemDados;
+	
+	int
+	numDoArqSaida;
+	
+	/* iterador comeca em 1 pq o argv[0] eh o nome do programa */
+	for ( int i = 1; i < argc; i++ )
+	{
+		/* verifica se é opcao */
+		if ( argv[i][0] == '-' )
+		{
+			/* verifica qual opcao */
+			switch ( argv[i][1] )
+			{
+				case 'J':
+					break;
+					
+				case 'D':
+					i++;
+					if ( !(i < argc -1) )
+					{
+						std::cout << "Numero insuficiente de parametros. " << 
+							"Digite 'mepa -?' para a lista completa de opcoes." << std::endl;
+						return 0;
+					}
+					if ( !atoi(argv[i]) )
+					{
+						std::cout 
+						<< "O parametro " << i+1 << " precisa ser um numero. " << 
+							"Digite 'mepa -?' para a lista completa de opcoes." << std::endl;
+						return 0;
+					}
+					numRegisBase = atoi(argv[i]);
+					break;
+					
+				case 'P':
+					i++;
+					if ( !(i < argc -1) )
+					{
+						std::cout << "Numero insuficiente de parametros. " << 
+							"Digite 'mepa -?' para a lista completa de opcoes." << std::endl;
+						return 0;
+					}
+					if ( !atoi(argv[i]) )
+					{
+						std::cout 
+						<< "O parametro " << i+1 << " precisa ser um numero. " << 
+							"Digite 'mepa -?' para a lista completa de opcoes." << std::endl;
+						return 0;
+					}				
+					tamMemPrograma = atoi(argv[i]);
+					break;
+					
+				case 'M':
+					i++;
+					if ( !(i < argc -1) )
+					{
+						std::cout << "Numero insuficiente de parametros. " << 
+							"Digite 'mepa -?' para a lista completa de opcoes." << std::endl;
+						return 0;
+					}
+					if ( !atoi(argv[i]) )
+					{
+						std::cout 
+						<< "O parametro " << i+1 << " precisa ser um numero. " << 
+							"Digite 'mepa -?' para a lista completa de opcoes." << std::endl;
+						return 0;
+					}
+					tamMemDados = atoi(argv[i]);
+					break;
+					
+				case 'I':
+					std::cout
+					<< "Alunos:" << std::endl
+					<< '\t' << "Leandro A. Boscariol" << std::endl
+					<< '\t' << "Lucas B. Gameiro" << std::endl;
+					return 0;
+					
+				case '?':
+					std::cout 
+					<< "Uso: mepa [opcoes] arquivo.mep" << std::endl
+					<< "Exemplo: mepa -D 5 foo.mep" << std::endl
+					<< std::endl
+					<< "Opcoes:" << std::endl
+					<< "-J" << "\t" << "Abre janela e mostra execucao do programa" << std::endl
+					<< "-D n" << "\t" << "Numero n de registradores base" << std::endl
+					<< "-P n" << "\t" << "Tamanho n em palavras de 16 bits da memoria de programa" << std::endl
+					<< "-M n" << "\t" << "Tamanho n em palavras de 16 bits da memoria de dados" << std::endl
+					<< "-I" << "\t" << "Identificacao da equipe" << std::endl
+					<< "-?" << "\t" << "Lista completa dos comandos" << std::endl
+					<< "-R foo" << "\t" << "Salvar no arquivo foo o resultado da instrucao MOSM. " <<
+							"Caso nao seja utilizado, o resultado e mostrado na saida padrao" << std::endl;
+					
+					return 0;
+					
+				case 'R':
+					i++;
+					if ( !(i < argc -1) )
+					{
+						std::cout << "Numero insuficiente de parametros. " << 
+							"Digite 'mepa -?' para a lista completa de opcoes." << std::endl;
+						return 0;
+					}
+					/* O construtor da MEPA recebera recebera o argv[numArqSaida] */
+					numDoArqSaida = i;
+					break;
+					
+				default:
+					std::cout << "Parametro desconhecido. " <<
+							"Digite 'mepa -?' para a lista completa de opcoes." << std::endl;
+					return 0;
+			
+			}
+		}
+		else if ( 1 )// verificar se o parametro é do tipo *.mep e iniciar a mepa.
+		{
+			
+		}
+		else
+		{
+			std::cout << "Tipo de arquivo invalido. " <<
+				"Digite 'mepa -?' para a lista completa de opcoes." << std::endl;
+			return 0;
+		}
+	}
+	
 	return 0;
 }
